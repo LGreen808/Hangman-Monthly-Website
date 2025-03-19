bd75af75ba5579cfc06e77190429c16bf4b702f8
LGreen808 <526LGreen@frhsd.com>
2025-03-19 14:25:47 +0000

    Working on the JavaScript

---
+++ script.js
@@ -0,0 +1,66 @@
+// Word List
+const wordList = [
+    'gold',
+    'luck',
+    'clover',
+    'green', 
+    'shamrock',
+    'leprechaun',
+    'march',
+    'greenery',
+    'tradition'
+]
+
+// Declare Variables
+let selectedWord = ''
+let displayedWord = ''
+let wrongGuesses = 0
+let guessedLetters = []
+const maxMistakes = 6
+
+
+
+
+// Start Game Function (runs everything)
+function startGame(level){
+    //reset game
+    wrongGuesses = 0
+    guessedLetters = []
+
+    selectedWord = getRandomWord(level) // Gets a random word based on the level the user selected
+
+    updateDifficultyDisplay(level) 
+}
+
+function getRandomWord(level) {
+    let filteredWords = wordList.filter ( word => {
+       if(level === 'easy') return word.length <= 4
+       if(level === 'medium') return word.length >= 5 && word.length <=7
+       if(level === 'hard') return word.length >= 8
+    })
+    
+    return filteredWords[Math.floor(Math.random()*filteredWords.length)]
+}
+
+//Update Difficulty Display
+function updateDifficultyDisplay(level){
+    let difficultyBox = document.getElementById('difficultyBox')
+    difficultyBox.classList.remove('easy', 'medium', 'hard')
+
+
+    if (level === 'easy') {
+       difficultyBox.classList.add('easy')
+       difficultyBox.textContent = 'Difficulty: Easy'
+    }
+    else if (level === 'medium'){
+        difficultyBox.classList.add('medium')
+        difficultyBox.textContent = 'Difficulty: Medium'
+
+    } else if(level === 'hard') {
+       difficultyBox.classList.add('hard')
+       difficultyBox.textContent = 'Difficulty: Hard'
+
+    }
+
+
+}
